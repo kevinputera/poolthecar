58129e7c1c784b3e528423adf07285cff864dd19
Kevin Putera <kevintiosanna@gmail.com>
2019-09-24 18:38:35 +0800

    Add 'Account' model

---
+++ server/models/account.js
@@ -0,0 +1,52 @@
+const { getClient } = require("../db");
+
+class Account {
+  constructor(email, secret, name, gender, phone, profile_photo_url) {
+    this.email = email;
+    // TODO: Hash secret and save it
+    this.secret = secret;
+    this.name = name;
+    this.gender = gender;
+    this.phone = phone;
+    this.profile_photo_url = profile_photo_url;
+  }
+
+  async save() {
+    const client = await getClient();
+    await client.query({
+      text: /* sql */ `
+        INSERT INTO accounts (email, secret, name, gender, phone, profile_photo_url)
+        VALUES ($1, $2, $3, $4, $5, $6)
+      `,
+      values: [
+        this.email,
+        this.secret,
+        this.name,
+        this.gender,
+        this.phone,
+        this.profile_photo_url
+      ]
+    });
+  }
+
+  static async findAll() {
+    const client = await getClient();
+    const accounts = await client.query(/* sql */ `
+      SELECT email, secret, name, gender, phone, profile_photo_url
+      FROM accounts
+    `);
+    return accounts.rows.map(
+      account =>
+        new Account(
+          account.email,
+          account.secret,
+          account.name,
+          account.gender,
+          account.phone,
+          account.profile_photo_url
+        )
+    );
+  }
+}
+
+module.exports = { Account };
