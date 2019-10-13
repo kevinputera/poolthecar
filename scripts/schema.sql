b217edab8ed841c554e3d253a36991c6a544ba9e
tejas123 <vasavada.tejas@gmail.com>
2019-10-13 15:26:39 +0800

    Add tables to represent trips and stops

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -9,6 +9,12 @@ CREATE TYPE gender AS ENUM (
   'non binary'
 );
 
+CREATE TYPE trip_status as ENUM (
+  'created'
+  'ongoing'
+  'finished'
+);
+
 CREATE TABLE accounts (
   email varchar(255) PRIMARY KEY,
   secret char(64) NOT NULL, -- SHA256 hash of the account's password
@@ -18,4 +24,25 @@ CREATE TABLE accounts (
   profile_photo_url varchar(1023) NOT NULL,
   created_on timestamptz NOT NULL DEFAULT NOW(),
   updated_on timestamptz NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE Trips (
+  tid integer PRIMARY KEY,
+  license varchar(255) NOT NULL, /*References Cars(license)*/
+  status trip_status NOT NULL,
+  origin varchar(100) NOT NULL,
+  seats int NOT NULL,
+  departing_on timestamptz NOT NULL DEFAULT NOW(),
+  
+  updated_on timestamptz NOT NULL DEFAULT NOW(),
+  CHECK (seats > 0)
+);
+
+CREATE TABLE StopsInTrip (
+  min_price numeric NOT NULL DEFAULT 0,
+  address varchar(100) NOT NULL,
+  tid integer,
+  CHECK (min_price >= 0),
+  PRIMARY KEY(tid,address),
+  FOREIGN KEY (tid) REFERENCES Trips ON DELETE cascade
 );
