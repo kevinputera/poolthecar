d5a54a95e90023ed5deeef0e97e8a81458449f85
JokerY <770792058@qq.com>
2019-10-13 12:06:54 +0800

    Add create table sql of Customers, Bids, Reviews

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -9,7 +9,13 @@ CREATE TYPE gender AS ENUM (
   'non binary'
 );
 
-CREATE TABLE accounts (
+CREATE TYPE bid_status AS ENUM (
+  'won',
+  'failed',
+  'pending'
+);
+
+CREATE TABLE Users (
   email varchar(255) PRIMARY KEY,
   secret char(64) NOT NULL, -- SHA256 hash of the account's password
   name varchar(255) NOT NULL,
@@ -18,4 +24,26 @@ CREATE TABLE accounts (
   profile_photo_url varchar(1023) NOT NULL,
   created_on timestamptz NOT NULL DEFAULT NOW(),
   updated_on timestamptz NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE Customers (
+  email varchar(255) PRIMARY KEY REFERENCES Users(email)
+);
+
+CREATE TABLE Bids (
+  email varchar(255) REFERENCES Customers(email),
+  tid varchar(255) /*REFERENCES Trips(tid)*/,
+  status bid_status NOT NULL,
+  value numeric NOT NULL,
+  created_on timestamptz NOT NULL DEFAULT NOW(),
+  updated_on timestamptz NOT NULL DEFAULT NOW(),
+  PRIMARY KEY (email, tid)
+);
+
+CREATE TABLE Reviews (
+  email varchar(255) REFERENCES Customers(email),
+  tid varchar(255) /* REFERENCES Trips(tid) */,
+  score numeric NOT NULL DEFAULT 5,
+  content text,
+  PRIMARY KEY (email, tid)
 );
