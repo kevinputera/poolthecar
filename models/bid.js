a70091c43e78d0790e10b676244cb4175a6fbb65
JokerY <770792058@qq.com>
2019-10-13 13:45:27 +0800

    Add bid class

--- models/bid.js
+++ models/bid.js
@@ -1,8 +1,6 @@
 const { getClient } = require('../db');
 
 class Bid {
-  static tableName = 'Bids';
-
   constructor(email, tid, status, value) {
     this.email = email;
     this.tid = tid;
@@ -13,9 +11,48 @@ class Bid {
   async add() {
     const client = await getClient();
     await client.query({
-      text: `INSERT INTO $1 (email, tid, status, value)
-            VALUES ($2, $3, $4, $5)`,
-      values: [tableName, this.email, this.tid, this.status, this.value],
+      text: /* sql */ `
+        INSERT INTO Bids (email, tid, status, value)
+        VALUES ($1, $2, $3, $4)
+      `,
+      values: [this.email, this.tid, this.status, this.value],
     });
   }
+
+  async update() {
+    const client = await getClient();
+    await client.query({
+      text: /* sql */ `
+        UPDATE Bids SET status = $3, value = $4
+        WHERE email = $1 AND tid = $2
+      `,
+      values: [this.email, this.tid, this.status, this.value],
+    });
+  }
+
+  async delete() {
+    const client = await getClient();
+    await client.query({
+      text: /* sql */ `
+        DELETE FROM Bids
+        WHERE email = $1 AND tid = $2
+      `,
+      values: [this.email, this.tid],
+    });
+  }
+
+  static async findAll() {
+    const client = await getClient();
+    const bids = await client.query({
+      text: /* sql */ `
+      SELECT email, tid, status, value
+      FROM Bids
+    `,
+    });
+    return bids.rows.map(
+      bid => new User(bid.email, bid.tid, bid.status, bid.value)
+    );
+  }
 }
+
+module.exports = { Bid: Bid };
