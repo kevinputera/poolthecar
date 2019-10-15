bda813c9ea88cd017a4450fbd358b566312b6343
ttippinyu <StartNow25>
2019-10-15 17:50:27 +0800

    id to mid

--- models/message.js
+++ models/message.js
@@ -1,8 +1,8 @@
 const { getClient } = require('../db');
 
 class Message {
-  constructor(id, sender, receiver, content, sent_on) {
-    this.id = id || 0;
+  constructor(mid, sender, receiver, content, sent_on) {
+    this.mid = mid || 0;
     this.sender = sender;
     this.receiver = receiver;
     this.content = content;
@@ -15,11 +15,11 @@ class Message {
       text: /* sql */ `
         INSERT INTO Messages (sender, receiver, content)
         VALUES ($1, $2, $3)
-        RETURNING id, sent_on
+        RETURNING mid, sent_on
       `,
       values: [this.sender, this.receiver, this.content],
     });
-    this.id = res.rows[0].id;
+    this.mid = res.rows[0].mid;
     this.sent_on = res.rows[0].sent_on;
     return this;
   }
@@ -28,7 +28,7 @@ class Message {
     const client = await getClient();
     await client.query(/* sql */ `
       DELETE FROM Messages
-      WHERE id = this.id
+      WHERE mid = this.mid
     `);
     return this;
   }
@@ -37,7 +37,7 @@ class Message {
     const client = await getClient();
     const messages = await client.query({
       text: /* sql */ `
-        SELECT id, sender, receiver, content, sent_on
+        SELECT mid, sender, receiver, content, sent_on
         FROM Messages
         WHERE (sender = $1 AND receiver = $2)
         OR    (sender = $2 AND receiver = $1)
@@ -48,7 +48,7 @@ class Message {
     return messages.rows.map(
       message =>
         new Message(
-          message.id,
+          message.mid,
           message.sender,
           message.receiver,
           message.content,
