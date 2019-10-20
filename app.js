6f955d0d1ef4fd44509f7a8eab2c0ad95d4d9783
Kevin Putera <kevintiosanna@gmail.com>
2019-10-20 16:43:36 +0800

    Use authentication routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const session = require('express-session');
 const { join } = require('path');
 
+const { authenticationRoutes } = require('./routes/authentication');
 const { userRoutes } = require('./routes/user');
 
 // Load env variables
@@ -10,8 +12,16 @@ const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', join(__dirname, 'views'));
+app.use(
+  session({
+    secret: process.env.SESS_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(express.urlencoded({ extended: false }));
 
+app.use('/', authenticationRoutes);
 app.use('/users', userRoutes);
 
 app.listen(process.env.APP_PORT, () =>
