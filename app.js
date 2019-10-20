5eaf4c4534460dd44ba3f9df0955332fc8374a87
Kevin Putera <kevintiosanna@gmail.com>
2019-10-20 15:31:44 +0800

    Use user route

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const { join } = require('path');
 
+const { userRoutes } = require('./routes/user');
+
 // Load env variables
 require('dotenv').config({ path: join(__dirname, '.env') });
 
 const app = express();
+
 app.set('view engine', 'pug');
 app.set('views', join(__dirname, 'views'));
-
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+app.use('/users', userRoutes);
 
 app.listen(process.env.APP_PORT, () =>
   console.log(`App started on port ${process.env.APP_PORT}!`)
