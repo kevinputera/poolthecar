0b0695a71153123c4e2396c4d7473421cd90d02a
JokerYan <770792058@qq.com>
2019-10-20 16:02:01 +0800

    Add test data and review in trip router

--- scripts/table.js
+++ scripts/table.js
@@ -1,6 +1,7 @@
 const { promises } = require('fs');
 const { join } = require('path');
 const { getClient } = require('../db');
+const { initTestData } = require('./testData');
 
 const SCHEMA_DIR = join(__dirname, 'schema.sql');
 
@@ -8,6 +9,7 @@ async function createTables() {
   const createTableString = (await promises.readFile(SCHEMA_DIR)).toString();
   const client = await getClient();
   await client.query(createTableString);
+  await initTestData();
 }
 
 createTables()
@@ -19,3 +21,5 @@ createTables()
     console.log('Table creation failed:', error);
     process.exit(1);
   });
+
+module.exports = { createTables };
