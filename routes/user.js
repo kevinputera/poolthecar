14b76e9a058244239e2e1660e13c2db84e9d17b6
Kevin Putera <kevintiosanna@gmail.com>
2019-10-22 20:22:47 +0800

    Add bookmark-related routes

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { User } = require('../models/user');
+const { Bookmark } = require('../models/bookmark');
 
 const router = express.Router();
 
@@ -16,8 +17,37 @@ router.post('/', async (req, res) => {
   const { email, secret, name, gender, phone, profilePhotoUrl } = req.body;
   try {
     const user = new User(email, secret, name, gender, phone, profilePhotoUrl);
-    await user.save();
-    res.json(user);
+    const savedUser = await user.save();
+    res.json(savedUser);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.post('/:email/bookmarks', async (req, res) => {
+  const email = req.params.email;
+  const { name, address } = req.body;
+  try {
+    const bookmark = new Bookmark(email, name, address);
+    const savedBookmark = await bookmark.save();
+    res.json(savedBookmark);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.put('/:email/bookmarks/:name', async (req, res) => {
+  const { email, name } = req.params;
+  const address = req.body.address;
+  try {
+    const bookmark = Bookmark.findByEmailAndName(email, name);
+    if (!bookmark) {
+      res.status(400).send('Bookmark does not exist');
+      return;
+    }
+    bookmark.address = address;
+    const updatedBookmark = await bookmark.update();
+    res.json(updatedBookmark);
   } catch (error) {
     res.status(500).send(error);
   }
