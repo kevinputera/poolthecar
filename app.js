7a05a184080ef63e5f9ce3d455e5b3a69746ea0e
Kevin Putera <kevintiosanna@gmail.com>
2019-11-02 21:58:03 +0800

    Implement main layout (#33)
    * Remove placeholder
    
    * Add main layout template
    
    * Add skeleton templates
    
    * Add page route
    
    * Use page route
    
    * Temporarily disable authentication
    
    * Add permanent redirection from '/' to '/p/trips'

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const { carRoutes } = require('./routes/car');
 const { messageRoutes } = require('./routes/message');
 const { driverRoutes } = require('./routes/driver');
 
+const { pageRoutes } = require('./routes/page');
+
 // Load env variables
 require('dotenv').config({ path: join(__dirname, '.env') });
 
@@ -28,15 +30,22 @@ app.use(
 );
 app.use(express.urlencoded({ extended: false }));
 
-// Authentication
-app.use('/', authenticationRoutes);
-app.use(requireAuthentication);
+// Permanent redirection
+app.get('/', (req, res) => {
+  res.status(301).redirect('/p/trips');
+});
 
-app.use('/users', userRoutes);
-app.use('/trips', tripRoutes);
-app.use('/cars', carRoutes);
-app.use('/messages', messageRoutes);
-app.use('/drivers', driverRoutes);
+// Authentication
+app.use('/api', authenticationRoutes);
+// app.use(requireAuthentication);
+
+app.use('/api/users', userRoutes);
+app.use('/api/trips', tripRoutes);
+app.use('/api/cars', carRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/drivers', driverRoutes);
+
+app.use('/p', pageRoutes);
 
 app.listen(process.env.APP_PORT, () =>
   console.log(`App started on port ${process.env.APP_PORT}!`)
