c1746002dafabd84ab4de188df0822c232b6c58e
Kevin Putera <kevintiosanna@gmail.com>
2019-11-02 23:26:18 +0800

    Implement account management page (#35)
    * Add account-related public assets
    
    * Add update query, remove unused queries
    
    * Add update route, remove unused routes
    
    * Prefer single quote over backtick
    
    * Supply current user to account view
    
    * Update account view

--- routes/page/account.js
+++ routes/page/account.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { User } = require('../../models/user');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  res.render('account', { title: 'Account' });
+  const currentUser = await User.findByEmail(req.session.email);
+  res.render('account', { title: 'Account', currentUser });
 });
 
 module.exports = { accountPageRoutes: router };
