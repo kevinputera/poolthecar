c431184582a556b415fd3089dca0e0f109de3ecd
Kevin Putera <kevintiosanna@gmail.com>
2019-11-03 17:38:29 +0800

    Remove isLoggedIn flag

--- routes/page/authentication.js
+++ routes/page/authentication.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/login', async (req, res) => {
-  res.render('login', { title: 'Log in', isLoggedIn: false });
+  res.render('login', { title: 'Log in' });
 });
 
 router.get('/signup', async (req, res) => {
-  res.render('signup', { title: 'Sign up', isLoggedIn: false });
+  res.render('signup', { title: 'Sign up' });
 });
 
 module.exports = { authenticationPageRoutes: router };
