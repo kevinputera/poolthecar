1fbedea610b3641ee395a3cce2d4028b941c9cda
Kevin Putera <kevintiosanna@gmail.com>
2019-11-06 15:52:12 +0800

    Add new stop form list appending logic

--- public/scripts/newTrip.js
+++ public/scripts/newTrip.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', () => {
-  // Trip creation logic
+  // Trip + stops creation logic
   const newTripForm = document.getElementById('new-trip-form');
   const newTripButton = document.getElementById('new-trip-button');
   newTripButton.addEventListener('click', async event => {
@@ -26,5 +26,56 @@ window.addEventListener('load', () => {
     } catch (error) {
       console.log('New trip creation error', error);
     }
+  });
+
+  // New stop form creation logic
+  let rowId = 2;
+  const stopFormList = document.getElementById('stop-form-list');
+  const newStopFormButton = document.getElementById('new-stop-form-button');
+  newStopFormButton.addEventListener('click', async event => {
+    event.preventDefault();
+
+    // Create row
+    const newStopForm = document.createElement('div');
+    newStopForm.className = 'form-row';
+
+    // Create address form group
+    const addressInputId = `stop-address-${rowId}`;
+    const addressFormGroup = document.createElement('div');
+    addressFormGroup.className = 'form-group col-md-6';
+    const addressFormLabel = document.createElement('label');
+    addressFormLabel.htmlFor = addressInputId;
+    addressFormLabel.innerHTML = `Address ${rowId}`;
+    const addressFormInput = document.createElement('input');
+    addressFormInput.id = addressInputId;
+    addressFormInput.className = 'form-control';
+    addressFormInput.name = 'stopAddress';
+    addressFormInput.type = 'text';
+    addressFormInput.placeholder = 'Enter address';
+    addressFormGroup.appendChild(addressFormLabel);
+    addressFormGroup.appendChild(addressFormInput);
+
+    // Create minimum price form group
+    const minPriceInputId = `stop-min-price-${rowId}`;
+    const minPriceFormGroup = document.createElement('div');
+    minPriceFormGroup.className = 'form-group col-md-6';
+    const minPriceFormLabel = document.createElement('label');
+    minPriceFormLabel.htmlFor = minPriceInputId;
+    minPriceFormLabel.innerHTML = `Minimum bid price ${rowId}`;
+    const minPriceFormInput = document.createElement('input');
+    minPriceFormInput.id = minPriceInputId;
+    minPriceFormInput.className = 'form-control';
+    minPriceFormInput.name = 'stopMinPrice';
+    minPriceFormInput.type = 'number';
+    minPriceFormInput.placeholder = 'Enter minimum bid price';
+    minPriceFormGroup.appendChild(minPriceFormLabel);
+    minPriceFormGroup.appendChild(minPriceFormInput);
+
+    newStopForm.appendChild(addressFormGroup);
+    newStopForm.appendChild(minPriceFormGroup);
+
+    stopFormList.appendChild(newStopForm);
+
+    rowId++;
   });
 });
